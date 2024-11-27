c24491fe09897f6c79498c644c57a915acbecabb
José Domingues <otiliojose227@gmail.com>
2024-11-27 16:51:21 -0300

    Add files via upload

---
+++ IMG/javascript.js
@@ -0,0 +1,23 @@
+import Typed from 'typed.js';
+
+var typed  = new Typed('#habilidades',
+    {
+        strings: [" ",
+            "Developer",
+           
+            "Designer",
+           
+            "Freelancer",
+           
+            "Blogger",
+           
+            " Youtuber"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        
+    }
+)
+    
